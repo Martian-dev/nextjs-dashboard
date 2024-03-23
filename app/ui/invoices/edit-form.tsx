4d1e9c28477c748442fd4173fa0526f7c23e2b5e
Vaibhav <52103891+Martian-dev@users.noreply.github.com>
2024-03-23 10:22:04 +0530

    added user authentication and login page

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -142,9 +142,11 @@ export default function EditInvoiceForm({
           </div>
         </fieldset>
         <div id="error" aria-live="polite" aria-atomic="true">
-          <p className="mt-2 text-sm text-red-500" key={state.message}>
-            {state.message}
-          </p>
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500" key={state.message}>
+              {state.message}
+            </p>
+          )}
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
